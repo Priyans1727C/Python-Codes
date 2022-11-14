43ad0cc69622ef44c0fc544e3a3ab70a38c4cd8d
Priyanshu Singh Rajput <ps032067@gmail.com>
2022-11-15 01:14:16 +0530

    adding  question in ex1_for_loop

--- for_loop/ex1_for_loop.py
+++ for_loop/ex1_for_loop.py
@@ -6,7 +6,7 @@ for i in range(1,N+1):
     if i%2==0:
         print(i)
 
-print()
+print()          #use to give empty line
 print()
 
 #  Write program to print odd number
@@ -15,3 +15,17 @@ N=int(input("N: "))
 for i in range(1,N+1):
     if i%2!=0:
         print(i)
+
+
+print()
+print()
+
+# Wrie the program to add n number of input by user
+print("For adding n number of inputs ")
+N=int(input("N: "))
+sum=0
+for i in range(N):
+    user_input= int(input("Enter Your Value: "))
+    sum=sum + user_input
+print(sum)
+
