ea6da1f4650127af8abe7ca70c71fcf7e5562cac
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2022-11-16 00:49:53 +0530

    added new pattern

--- pattern/prymaid.py
+++ pattern/prymaid.py
@@ -10,3 +10,15 @@ for i in range(a):
     for k in range(a-i):
         print("*",end=" ")
     print()
+
+    print()
+print()
+
+# for upper pyramid
+
+for i in range(a):
+    for k in range(a-i-2,-1,-1):
+        print(" ",end="")
+    for l in range(i+1):
+        print("*",end=" ")
+    print()
