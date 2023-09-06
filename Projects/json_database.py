3c9eb56039b421cec0e6eaa81099b3b7669003d6
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-09-06 14:52:13 +0530

    Update json_database.py
    adding update function

--- Projects/json_database.py
+++ Projects/json_database.py
@@ -36,8 +36,33 @@ def insertion(temp_data):
                 data[key]=value
             f.write(json.dumps(data,indent=4))
 
+def update():
+    print()
+    reg_no=input('    Reg_no     : ')
+    name=input('    Name       : ')
+    occupation=input('    Occupation : ')
+    dob=input('    DOB        : ')
+    value={'Name':name,'Occupation':occupation,'DOB':dob}
+    try:
+        with open("database.json",'r') as f:
+            temp=f.read()
+            data=json.loads(temp)
+        if(reg_no in data):
+            with open("database.json",'w') as f:
+                data[reg_no]=value
+                f.write(json.dumps(data,indent=4))
+        else:
+            print("NO such reg.no exist")
+    except Exception as f:
+        print('NO data found!')
+        print(f)
+ 
 
-
-
-temp_data=inputs()
-insertion(temp_data)
+choice=int(input('choice: '))
+if(choice==1):
+    temp_data=inputs()
+    insertion(temp_data)
+elif(choice==2):
+    update()
+else:
+    print("Enter valid choice")
