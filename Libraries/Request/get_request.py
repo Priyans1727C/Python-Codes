22b387725262e3d5f46e457dec0706082f77b073
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-11-25 21:58:48 +0530

    Create get_request.py

---
+++ Libraries/Request/get_request.py
@@ -0,0 +1,24 @@
+import requests as req
+
+url="https://httpbin.org/basic-auth/user/pass"
+
+
+#normal get request
+# response=req.get(url)
+
+#Get request with authentication
+response=req.get(url,auth=('user','pass'))
+
+print(response.status_code)
+
+print(response.json())
+
+
+url2= "https://httpbin.org/get"
+payload={'key':'value','key2':'value2'}
+
+r=req.get(url2,params=payload)
+
+print(r.url)
+
+print(r.text)
