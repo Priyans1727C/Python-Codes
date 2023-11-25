d47759b7b90b913d88298f49a5ab73b95bb7b93b
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-11-25 22:00:27 +0530

    Create post_request.py

---
+++ Libraries/Request/post_request.py
@@ -0,0 +1,19 @@
+import requests as req
+import json
+
+post_req_url= "https://httpbin.org/anything"
+
+
+r=req.get(post_req_url)
+
+#by default it contain json file
+print(r.text)
+
+#printing in json format
+print(r.json())
+
+#making post request here
+post_req= req.post(post_req_url,data={'name':'thakur','name2':'ankur'})
+
+
+print(post_req.text)
