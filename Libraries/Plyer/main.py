b4354bd637662bdbce368bf2a9c32fc06f3aa4d3
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-11-29 21:21:18 +0530

    Create main.py

---
+++ Libraries/Plyer/main.py
@@ -0,0 +1,15 @@
+from plyer import notification
+
+
+def send_notification():
+    notification.notify(
+        
+        title="My List",
+        message="HELLO BUDDY!!!",
+        app_icon="notification_bell.ico",
+        app_name="Notifier",
+        timeout=10
+        
+    )
+    
+send_notification()
