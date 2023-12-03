ad61dd3a49af5e1f4a09e6d164e425fc8470f96e
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-12-03 19:39:51 +0530

    Update 2D_matrix.py

--- for_loop/2D_matrix.py
+++ for_loop/2D_matrix.py
@@ -15,3 +15,16 @@ for row in matrix:
     for element in row:
         print(element, end=' ')
     print()
+
+
+# Calculate and display the sum of all elements in the matrix
+matrix_sum = sum(sum(row) for row in matrix)
+print(f"Sum of all elements in the matrix: {matrix_sum}")
+
+# Calculate and display the transpose of the matrix
+transpose_matrix = [[matrix[j][i] for j in range(rows)] for i in range(columns)]
+print("Transpose of the matrix:")
+for row in transpose_matrix:
+    for element in row:
+        print(element, end=' ')
+    print()
