23c1ac784d99a7fa2aab98ad898a138652daf8ef
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-12-06 22:56:35 +0530

    Update 1_list.py

--- intro_List.py/1_list.py
+++ intro_List.py/1_list.py
@@ -1,16 +1,21 @@
-#list is a number of arguments
-list = [1,4,5,6,7,5] 
-list2 = ["ram","harsh","Subham"]
-list3 = ["ram",46,"yjugr",2.00,"ehgrg"]
+# Original lists
+list = [1, 4, 5, 6, 7, 5]
+list2 = ["ram", "harsh", "Subham"]
+list3 = ["ram", 46, "yjugr", 2.00, "ehgrg"]
 
-#list can contain any data type 
-print("list:",list)
-print("list2:",list2)
-print("list3:",list3)
+# Display original lists
+print("Original list:", list)
+print("Original list2:", list2)
+print("Original list3:", list3)
 
-#Indexing
-print("list[0]:",list[0])
-print("list[1]:",list[1])
-#and more up to length of list
-print("list[-1]:",list[-1])
-print("list[-2]:",list[-2])
+# Function to calculate the sum of a list
+def calculate_sum(input_list):
+    if all(isinstance(x, (int, float)) for x in input_list):
+        return sum(input_list)
+    else:
+        return "Cannot calculate sum for non-numeric elements."
+
+# Calculate and print the sum of each list
+print("Sum of list:", calculate_sum(list))
+print("Sum of list2:", calculate_sum(list2))
+print("Sum of list3:", calculate_sum(list3))
