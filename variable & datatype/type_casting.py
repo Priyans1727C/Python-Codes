275279e3db26000138dd8777115c4d956e65dcb9
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-12-08 19:33:46 +0530

    Update type_casting.py

--- variable & datatype/type_casting.py
+++ variable & datatype/type_casting.py
@@ -1,10 +1,24 @@
-'''We can change data type of any variable if possible '''
+def add_feature(variable):
+    # Check the current data type
+    original_type = type(variable)
+
+    # Perform the data type conversion
+    if original_type == int:
+        variable = str(variable)
+    elif original_type == str:
+        # Add the feature for strings (e.g., concatenate a string)
+        variable += " NewFeature"
+
+    # Print the new data type
+    print(f"New data type of variable: {type(variable)}")
+
+    # Print the variable with the added feature
+    print(f"Variable with added feature: {variable}")
 
 a = 30
 b = "Harsh"
-print(type(a))     #here 'a' is an integers
+print(type(a))  
 
-#Changeing data type
- 
-a=str(a)       #changed to strings 
-print(type(a))
+
+add_feature(a)
+add_feature(b)
