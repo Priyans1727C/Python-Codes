fafabf31006a8da00d0bae733ceee1aab5c4c3b1
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-12-11 21:29:04 +0530

    Update string_testing.py

--- strings/string_testing.py
+++ strings/string_testing.py
@@ -1,14 +1,19 @@
-print()
-print("Type '0' to end the loop")
-print("")
-while True:
-    a=input("Type your string: ")
-    if a=="0":
-        break
-    elif a.isalpha()==True:
-        print("Result: Yes this is alphabet")
-    elif a.isdigit()==True:
-        print("Result: This is Digit ")
-    elif a.isalnum()==True:
-        print("Result: This is Alpha-numeric")
+print("Type '0' to end the loop\n")
 
+while True:
+    user_input = input("Type your string: ")
+
+    if user_input == "0":
+        break
+    elif user_input.isalpha():
+        print("Result: Yes, this is an alphabet")
+    elif user_input.isdigit():
+        print("Result: This is a digit")
+    elif user_input.isalnum():
+        print("Result: This is alpha-numeric")
+    
+    # Check if the input is a palindrome
+    if user_input.lower() == user_input.lower()[::-1]:
+        print("Result: This is a palindrome")
+    else:
+        print("Result: This is not a palindrome")
