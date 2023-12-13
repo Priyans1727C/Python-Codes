66d57edeabe4f2b4ef707e4703d9e7cc982f7ea7
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-12-13 21:12:39 +0530

    Update json_in_file.py

--- json/json_in_file.py
+++ json/json_in_file.py
@@ -1,22 +1,30 @@
 import json
 
-table={
-    'data1':{
-              'id':1,
-              'name':'priyanshu',
-              'class':9        
+def read_and_print_json(file_path):
+    with open(file_path, "r") as f:
+        json_data = json.load(f)
+
+    # Print the JSON data with an indentation of 7
+    print(json.dumps(json_data, indent=7))
+
+# Your existing code
+table = {
+    'data1': {
+        'id': 1,
+        'name': 'priyanshu',
+        'class': 9
     },
-    
-    'data2':{
-              'id':2,
-              'name':'harsh',
-              'class':10        
-    }   
-    
+    'data2': {
+        'id': 2,
+        'name': 'harsh',
+        'class': 10
+    }
 }
 
-data=json.dumps(table,indent=5)
+data = json.dumps(table, indent=5)
 
-with open("file.json","w") as f:
+with open("file.json", "w") as f:
     f.write(data)
-# print(json.dumps(table,indent=7))
+
+# Call the new function to read and print the JSON data
+read_and_print_json("file.json")
